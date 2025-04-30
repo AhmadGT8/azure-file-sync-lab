1c52a75d91d00d857042f6a69ed82b9daee4e26a
Ahmad Talafha - MSFT <90010336+AhmadGT8@users.noreply.github.com>
2025-04-30 03:20:59 +0300

    Update install-and-configure-agent.ps1

--- install-and-configure-agent.ps1
+++ install-and-configure-agent.ps1
@@ -1,43 +1,40 @@
 param(
-  [string] $syncServiceName,
-  [string] $syncGroupName,
-  [string] $serverEndpointPath,
+  [string] $storageSyncSvc,
+  [string] $storageAccountName,
+  [string] $azureFileShareName,
+  [string] $serverLocalPath,
   [string] $location,
-  [string] $fileShareName
+  [string] $syncGroupName
 )
 
-# 1) Prepare server (disable IE ESC, etc)
-$installType = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").InstallationType
-if ($installType -ne "Server Core") {
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -Force
-  Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -Force
+# 1) Prepare Windows Server for Azure File Sync
+$installType = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -Name 'InstallationType').InstallationType
+if ($installType -ne 'Server Core') {
+  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -Name 'IsInstalled' -Value 0 -Force
+  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' -Name 'IsInstalled' -Value 0 -Force
   Stop-Process -Name iexplore -ErrorAction SilentlyContinue
 }
 
-# 2) Download & install the MSI for Server 2019
-Invoke-WebRequest -Uri https://aka.ms/afs/agent/Server2019 -OutFile "StorageSyncAgent.msi"
-Start-Process -FilePath "StorageSyncAgent.msi" -ArgumentList "/quiet" -Wait
-Remove-Item -Path ".\StorageSyncAgent.msi" -Force
+# 2) Create the folder to sync
+if (-Not (Test-Path -Path $serverLocalPath)) {
+  New-Item -ItemType Directory -Force -Path $serverLocalPath
+}
 
-# 3) Create the local folder
-New-Item -ItemType Directory -Force -Path $serverEndpointPath
+# 3) Download & silently install Azure File Sync Agent for Server 2019
+Invoke-WebRequest -Uri https://aka.ms/afs/agent/Server2019 -OutFile 'StorageSyncAgent.msi'
+Start-Process -FilePath 'msiexec.exe' -ArgumentList '/i StorageSyncAgent.msi','/quiet' -Wait
 
-# 4) Register the agent & create server endpoint
-& "$Env:ProgramFiles\Azure\StorageSyncAgent\RegisterStorageSyncAgent.exe" /ServerEndpoint:$syncGroupName:$serverEndpointPath
-
-# 5) (After cloud endpoint exists) register server endpoint
-New-Item -ItemType Directory -Force -Path "$serverEndpointPath\_tmp" | Out-Null
-$regKey = (az storage sync sync-group server endpoint generate-key `
-  --resource-group $env:RESOURCEGROUP `
-  --storage-sync-service-name $syncServiceName `
-  --sync-group-name $syncGroupName `
-  --name "cloudEndpoint").primaryKey
-Register-AzStorageSyncServerEndpoint `
-  -ResourceGroupName $env:RESOURCEGROUP `
-  -StorageSyncServiceName $syncServiceName `
+# 4) Register this machine as a Server Endpoint
+$rgName = $env:RESOURCE_GROUP
+Connect-AzAccount -Identity
+$storageAccountResourceId = (Get-AzStorageAccount -ResourceGroupName $rgName -Name $storageAccountName).Id
+New-AzStorageSyncServerEndpoint `
+  -ResourceGroupName $rgName `
+  -StorageSyncServiceName $storageSyncSvc `
   -SyncGroupName $syncGroupName `
-  -ServerEndpointName $syncGroupName `
-  -ServerLocalPath $serverEndpointPath `
-  -StorageAccountResourceId (Get-AzStorageAccount -ResourceGroupName $env:RESOURCEGROUP -Name $env:STORAGEACCOUNTNAME).Id `
-  -AzureFileShareName $fileShareName `
-  -AuthenticationKey $regKey
+  -Name 'serverEndpoint' `
+  -StorageAccountResourceId $storageAccountResourceId `
+  -AzureFileShareName $azureFileShareName `
+  -ServerLocalPath $serverLocalPath
+
+Write-Host 'Server Endpoint registered.'
